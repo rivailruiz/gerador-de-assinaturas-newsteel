408dcc981d6ddb214118088598cc59801594333a
Alexander Ruiz <AlexanderRuiz@Alexanders-MacBook-Air.local>
2017-12-11 22:49:06 -0200

    fix

---
+++ script_ruiz.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    
+    $('#refresh').on('click', function(e){
+        let nome = $('#name').val();
+        let cel = $('#cellphone').val();
+        let cargo = $('#cargo').val();
+
+        $('.nome').text(nome);
+        $('.tel span').text('Tel: +55 '+cel);
+        $('.cargo').text(cargo);
+        e.preventDefault();
+    });
+});
